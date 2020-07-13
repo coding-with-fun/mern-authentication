44495a7679c49a5f4a9703ba7edcab00d50dddf1
Harsh Patel <harshp2482@gmail.com>
2020-07-13 12:41:23 +0530

    Profile page tweaks

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,14 +1,31 @@
 import React, { useContext } from "react";
 import { UserContext } from "../../contexts/UserContext";
+import "./Profile.css"
 
 export default function Profile() {
   const { userData } = useContext(UserContext);
   return (
-    <div className="container">
-      <div className="card">
-        <h1 className="card-header">{userData.user.name}</h1>
-        <div className="card-body">
-          <p className="card-text">{userData.user.email}</p>
+    <div className="profile">
+      <div className="container">
+        <div className="card">
+          <h1 className="card-header">{userData.user.name}</h1>
+          <div className="card-body">
+            <p className="card-text">{userData.user.email}</p>
+          </div>
+          <div>
+            <a
+              href="/"
+              class="btn btn-primary"
+            >
+              Update Account
+            </a>
+            <a
+              href="/"
+              class="btn btn-primary"
+            >
+              Delete Account
+            </a>
+          </div>
         </div>
       </div>
     </div>
